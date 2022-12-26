fb53e3fc96ba3b16f3be6d09362dec4c7cf03441
forkingbeardman <115569419+forkingbeardman@users.noreply.github.com>
2022-12-26 15:08:48 +0400

    updates
    Added :
    1. Supports Old Georgian ("Asomtavruli") and Mid Georgian ("Nuskha-khutsuri") scripts
    2. Supports adding random numbers
    3. Supports random punctuation marks
    4. Word limit dropdown

--- index.php
+++ index.php
@@ -1,6 +1,42 @@
 <?php
 require __DIR__ . '/src/def.php';
 header('Content-Type: text/html; charset=utf-8');
+
+$text = '<div class="dummy">ლორემ იპსუმმა რა დაგიშავა? არაა, უნდა მაწვალო!</div>';
+$chars = "";
+$result = "";
+//options
+$aDropd = array("100", "200", "500");
+//$styles = array("mtavruli"); //for future autogenerating
+$addOps = array("mtavruli" => "off", "am" => "off", "nk" => "off", "punc" => "off", "wordcount" => "100", "numbers" => "off");
+
+//post
+if (isset($_POST['submit'])) {
+	//be sure to validate and clean your variables
+	$chars = htmlentities($_POST['chars']);
+	if (isset($_POST['mtavruli'])) {
+		$addOps['mtavruli'] = htmlentities($_POST['mtavruli']);
+	}
+	if (isset($_POST['am'])) {
+		$addOps['am'] = htmlentities($_POST['am']);
+	}
+	if (isset($_POST['nk'])) {
+		$addOps['nk'] = htmlentities($_POST['nk']);
+	}
+	if (isset($_POST['wordcount'])) {
+		$addOps['wordcount'] = htmlentities($_POST['wordcount']);
+	}
+	if (isset($_POST['numbers'])) {
+		$addOps['numbers'] = htmlentities($_POST['numbers']);
+	}
+	if (isset($_POST['punc'])) {
+		$addOps['punc'] = htmlentities($_POST['punc']);
+	}
+
+	//then you can use them in a PHP function. 
+	$result = my_text_finder($chars, $addOps);
+}
+
 ?>
 
 <head>
@@ -10,29 +46,46 @@ header('Content-Type: text/html; charset=utf-8');
 	<meta name="author" content="ForkingBeardman">
 	<meta name="description" content="Generate Random Georgian words using only the characters provided">
 	<link rel="stylesheet" type="text/css" href="src/style.css">
-</head>
 
 <body>
+	</head>
 	<div class="wrapper">
 		<div class="user-input">
-			<form action="" method="get">
+			<form action="" method="post" id="">
 				<div class="input-field">
-					<input type="text" class="form-control" name="chars" id="chars" placeholder="ასოები..." autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false"></input>
+					<input type="text" class="form-control" name="chars" id="chars" placeholder="ასოები..." autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" value="<?php echo clean_chars($chars) ?>"></input>
 					<div class="underline"></div>
 				</div>
 				<div class="buttons-wrap">
 					<div class="buttons">
-						<div class="btn">
-							<div class="btn-group">
-								<input type="submit" name="submit" value="ძიება"></input>
-							</div>
-						</div>
+						<input type="submit" name="submit" value="ძიება" class="btn"></input>
 						<div class="btn">
 							<a href="./">
 								<div class="clear">გასუფთავება</div>
 							</a>
 						</div>
 					</div>
+				</div>
+				<div class="add-options">
+					<label class="lbl_chckbox" for="mtavruli">მთავრული</label>
+					<input type="checkbox" name="mtavruli" id="mtavruli" <?php echo $addOps['mtavruli'] <> "off" ? "checked" : "" ?>>
+					<label class="lbl_chckbox" for="am">ასომთავრული</label>
+					<input type="checkbox" name="am" id="am" <?php echo $addOps['am'] <> "off" ? "checked" : "" ?>>
+					<label class="lbl_chckbox" for="nk">ნუსხური</label>
+					<input type="checkbox" name="nk" id="nk" <?php echo $addOps['nk'] <> "off" ? "checked" : "" ?>>
+					<label class="lbl_chckbox" for="numbers">რიცხვები</label>
+					<input type="checkbox" name="numbers" id="numbers" <?php echo $addOps['numbers'] <> "off" ? "checked" : "" ?>>
+					<label class="lbl_chckbox" for="punc">პუნქტუაცია</label>
+					<input type="checkbox" name="punc" id="punc" <?php echo $addOps['punc'] <> "off" ? "checked" : "" ?>>
+					<label for="wordcount">მაქს რაოდენობა:</label>
+					<?php
+					echo '<select id="wordcount" name="wordcount">';
+					foreach ($aDropd as $sOption) {
+						$sSel = ($sOption == htmlentities($_POST['wordcount'])) ? 'selected="selected"' : "";
+						echo "<option   $sSel>$sOption</option>";
+					}
+					echo "</select>";
+					?>
 				</div>
 			</form>
 		</div>
@@ -41,21 +94,8 @@ header('Content-Type: text/html; charset=utf-8');
 				<!-- Submit Logic -->
 
 				<?php
-				$text = '<div class="dummy">ლორემ იპსუმმა რა დაგიშავა? არაა, უნდა მაწვალო!</div>';
-				$val1 = "";
-				$result = "";
-				if (isset($_GET['submit'])) {
-					//be sure to validate and clean your variables
-					$val1 = htmlentities($_GET['chars']);
-					//echo $val1;
-					$text = "";
-					//then you can use them in a PHP function. 
-					$result = my_text_finder($val1);
-					foreach ($result as $res) {
-						$text .=  $res . " ";
-					}
-				}
-				echo $text;
+
+				echo $result;
 				?>
 			</div>
 		</div>
--- src/def.php
+++ src/def.php
@@ -1,20 +1,42 @@
 <?php
 //definictions and locations
 define('listFileLocation', __DIR__ . "/ka_GE.txt");
-define('listLetters', __DIR__ . "/ka_GE_Letters.txt");
 
 
 //split by chars and remove non-allowed
 function my_char_split($string)
 {
+
     $letters = get_letters(); //get allowed list
+    include(__DIR__ . '/dicts.php');
+
+    foreach ($mt_conv as $a => $val) {
+        $string = str_replace($a, $val, $string);
+    }
     $arr_str = mb_str_split($string); //do da split
     $arr_str = array_unique($arr_str); //keep unique only
 
     $result = array_intersect($letters, $arr_str); //leave only allowed
 
     $result = array_map("trim", $result);
+
+
+
+
     return $result;
+}
+
+//randomly provide true false
+function random($randomness)
+{
+    $randomness = $randomness * 10;
+    $tf = rand(0, $randomness) == 1;
+    return $tf;
+}
+
+function clean_chars($string)
+{
+    return implode("", my_char_split($string));
 }
 
 
@@ -32,19 +54,15 @@ function get_words()
     return $result;
 }
 
-//get allowed letter dict
 function get_letters()
 {
+    include(__DIR__ . '/dicts.php');
     $result = array();
-    $letterlist = fopen(listLetters, "r") or die("Unable to locate/open the word list file.");
-    while (($l = fgets($letterlist)) !== false) {
-        array_push($result, $l);
-    }
+    $result = $georgian_letters;
 
     $result = array_map("trim", $result);
     return $result;
 }
-
 //match words
 function word_match($word, $char)
 {
@@ -72,9 +90,11 @@ function word_match($word, $char)
 
 
 //main logic
-function my_text_finder($string)
+function my_text_finder($string, $addOps)
 {
-    $dummy = '<div class="dummy">ჩაგეწერა ჯერ რამე შე კაი ადამიანო!</div>'; //text to display if nothing searched
+    include(__DIR__ . '/dicts.php');
+    $text = "";
+    $matched = 0;
     if ($string <> "") {
         $result = array();
         $wordlist = array();
@@ -86,21 +106,88 @@ function my_text_finder($string)
         $matches = word_match($wordlist, $substring);  //check dictionary
 
         //set max word amount and make sure there's at least on result else return dummy html
-        $word_count_limit = 200;
+        $word_count_limit = $addOps["wordcount"];
         $arr_size_max = count($matches);
         if ($arr_size_max > $word_count_limit) {
             $arr_size_max = $word_count_limit;
         }
-        
+
         if ($arr_size_max > 1) {
             $rand_key = array_rand($matches, $arr_size_max);
-            shuffle($rand_key);
+
             foreach ($rand_key as $k) {
                 array_push($result, $matches[$k]);
             }
+
+            if ($addOps["numbers"] <> "off") {
+                $maxNum = $arr_size_max / 20;
+                for ($i = 0; $i <= $maxNum; $i++) {
+                    array_push($result, rand(0, 9999));
+                }
+            }
+            shuffle($result);
         }
-    } else {
-        $result = array($dummy);
+
+        $matched = count($result);
     }
-    return $result;
+
+    $punc = $addOps["punc"];
+
+
+
+
+    if ($matched > 0) { //start of mathed results
+        foreach ($result as $res) {
+            if ($punc <> "off" && random(5)) {
+                $res = '"' . $res . '"';
+            }
+            if ($punc <> "off" && random(5)) {
+                $res = "(" . $res . ")";
+            }
+            if ($punc <> "off" && random(10)) {
+                $res = "[" . $res . "]";
+            }
+            if ($punc <> "off" && random(10)) {
+                $res = "{" . $res . "}";
+            }
+            if ($punc <> "off" && random(3)) {
+                $k = array_rand($punctuation_marks);
+                $v = $punctuation_marks[$k];
+                $res =  $res . $v;
+            }
+            $text .=  $res ." ";
+        }
+        $f_text = $text;
+        //check for additional options
+        if ($addOps["mtavruli"] <> "off") {
+            $f_text .= "<br/><br/>";
+            $mt_text = $text;
+            foreach ($mt_conv as $a => $val) {
+                $mt_text = str_replace($val, $a, $mt_text);
+            }
+            $f_text .= $mt_text;
+        }
+        if ($addOps["am"] <> "off") {
+            $f_text .= "<br/><br/>";
+            $am_text = $text;
+            foreach ($am_conv as $a => $val) {
+                $am_text = str_replace($val, $a, $am_text);
+            }
+            $f_text .= $am_text;
+        }
+        if ($addOps["nk"] <> "off") {
+            $f_text .= "<br/><br/>";
+            $nk_text = $text;
+            foreach ($nk_conv as $a => $val) {
+                $nk_text = str_replace($val, $a, $nk_text);
+            }
+            $f_text .= $nk_text;
+        }
+    } else { //end of mathed results
+        $f_text = '<div class="dummy">ჩაგეწერა ჯერ რამე შე კაი ადამიანო!</div>'; //text to display if nothing searched
+
+    }
+
+    return $f_text;
+    //return array_rand($punctuation_marks) ;
 }
