b814490b801449784242dafa957cf2ca37f8c38a
forkingbeardman <115569419+forkingbeardman@users.noreply.github.com>
2023-02-26 20:20:53 +0400

    reversed language logic to fix bug

--- views/languagedrop.view.php
+++ views/languagedrop.view.php
@@ -1,8 +1,9 @@
-<form action="<?= $_SERVER['PHP_SELF']; ?>" method="POST">
-    <select id="lang" name="lang" class = "dropdown"onchange="this.form.submit();">
-        <?php foreach (get_languages() as $lang => $lang_file) : ?>
-            <?php $sSel = $lang == $vb['lang'] ? 'selected="selected"' : ""; ?>
-            <option value="<?= $lang ?>" <?= $sSel?>><?= _l($lang) ?></option>
-        <?php endforeach ?>
-    </select>
+<form id="lang" action="<?= $_SERVER['PHP_SELF']; ?>" method="POST">
+<select id="language" onchange="window.location.href = this.value;">
+    <?php foreach (get_languages() as $lang => $lang_file) : ?>
+        <?php $sSel = $lang == $vb['lang'] ? 'selected="selected"' : ""; ?>
+        <option value="?lang=<?= $lang ?>" <?= $sSel?>><?= _l($lang) ?></option>
+    <?php endforeach ?>
+</select>
+</div>
 </form>
--- views/layout.view.php
+++ views/layout.view.php
@@ -8,7 +8,12 @@
     <meta name="author" content="ForkingBeardman">
     <meta name="description" content="Generate Random Georgian words using only the characters provided">
 </head>
-
+<script>
+    const select = document.querySelector('#language-select');
+    select.addEventListener('change', function() {
+        window.location.href = `?lang=${this.value}`;
+    });
+</script>
 <body>
 
     <body>
@@ -22,7 +27,7 @@
                         </div>
                         <?php require("options.view.php"); ?>
                         <div class="box">
-                            <input type="submit" name="submit" value="<?= _l("action") ?>" class="btn">
+                            <input type="submit" name="generate" value="<?= _l("action") ?>" class="btn">
                         </div>
                     </form>
                 </div>
@@ -49,8 +54,8 @@
             <div class="footer">
                 © 2022. GRS
             </div>
-            <!-- <div class="reset">
-                <a id="refresh" value="Refresh" href="./">
+            <div class="reset">
+                <a id="refresh" value="Refresh" href="./?lang=<?= set_language() ?>">
                     <svg class="refreshicon" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="25px" height="25px" viewBox="0 0 322.447 322.447" style="enable-background:new 0 0 322.447 322.447;" xml:space="preserve">
                         <path d="M321.832,230.327c-2.133-6.565-9.184-10.154-15.75-8.025l-16.254,5.281C299.785,206.991,305,184.347,305,161.224
                             c0-84.089-68.41-152.5-152.5-152.5C68.411,8.724,0,77.135,0,161.224s68.411,152.5,152.5,152.5c6.903,0,12.5-5.597,12.5-12.5
@@ -60,7 +65,7 @@
                             l45.369-14.738C320.371,243.946,323.965,236.895,321.832,230.327z" />
                     </svg>
                 </a>
-            </div> -->
+            </div>
         </div>
     </body>
 
